7b1ccac6d29bd40d57634dceb504ba8a5edf5697
Maksym Liashevskyi <maks.liashevskyi@gmail.com>
2024-08-21 11:03:28 +0200

    disabled 404 page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react"
 
 export default function NotFound(){
-  useEffect(() => {
-    window.location.href = '/'
-  }, [])
+  // useEffect(() => {
+  //   window.location.href = '/'
+  // }, [])
   return(
     <p style={{marginBottom: '400px', fontSize: '40px'}}>404 not found</p>
   )
